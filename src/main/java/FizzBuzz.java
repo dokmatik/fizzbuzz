40851d8e0b56cd0ea5038fa0b946fcaa3ac1af30
mgoktay <metin@goktay.de>
2018-06-04 21:52:22 +0200

    Javadoc

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,12 @@
  * Created by dokmatik on 04.06.18.
  */
 public class FizzBuzz {
+    /**
+     * Implementirung der FizzBuzz Funktion.
+     *
+     * @param i Eine Ganzzahl
+     * @return Sofern i durch 3 teilbar ist, wird "Fizz" zurückgegeben. Sofern i durch 5 teilbar ist, wird "Buzz" zurückgegeben. Ist i sowohl durch 3 als auch 5 teilbar, wird "FizzBuzz" zurückgegeben. In allen anderen Fällen wird die Einganszahl als Text zurückgegeben.
+     */
     public String fizzBuzz(int i) {
         String returnValue = "";
         boolean teilbarDurch3 = i % 3 == 0;
