fa6d7f5b4ed566c6fef9eb42b639c2d8e3c4c47b
mgoktay <metin@goktay.de>
2018-06-04 21:38:42 +0200

    Weitere Tests -> GRÜN

--- src/test/java/TestFizzBuzz.java
+++ src/test/java/TestFizzBuzz.java
@@ -15,5 +15,11 @@ public class TestFizzBuzz {
         assertEquals(fizzBuzz.fizzBuzz(15), "FizzBuzz");
         assertEquals(fizzBuzz.fizzBuzz(1), "1");
         assertEquals(fizzBuzz.fizzBuzz(2), "2");
+        assertEquals(fizzBuzz.fizzBuzz(6), "Fizz");
+        assertEquals(fizzBuzz.fizzBuzz(7), "7");
+        assertEquals(fizzBuzz.fizzBuzz(8), "8");
+        assertEquals(fizzBuzz.fizzBuzz(9), "Fizz");
+        assertEquals(fizzBuzz.fizzBuzz(10), "Buzz");
+        assertEquals(fizzBuzz.fizzBuzz(11), "11");
     }
 }
